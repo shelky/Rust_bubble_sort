0ad650b6199e59be0680ad446b854872e5570664
Artur Miroszewski <a.miroszewski@gmail.com>
2021-05-08 16:58:20 +0200

    Add generating array function

--- src/main.rs
+++ src/main.rs
@@ -28,31 +28,28 @@ fn bubble_sort(arr: &mut [i32]) {
     }
 }
 
-
+fn generate_array(n: usize) -> Vec<i32> {
+    println!("{}", n);
+    let mut b = vec![0; n];
+    for i in 0..n {
+        b[i]=rand::thread_rng().gen_range(1..10);
+    }
+    b
+}
 
 fn main() {
+
     println!("State array size: ");
-
     let mut size = String::new();
-
     io::stdin()
         .read_line(&mut size)
         .expect("Failed to read line");
-
     let size: usize = size.trim().parse().expect("Please type a number!");
-
     println!("Chosen array size: {}", size);
 
-    //const ARRAY_SIZE:usize = 10;
-    //let mut b: [i32; ARRAY_SIZE] = [0;ARRAY_SIZE];
-    let mut b = vec![0; size];
-    //b[0] = rand::thread_rng().gen_range(1..10);
-    for i in 0..size {
-        b[i]=rand::thread_rng().gen_range(1..10);
-    }
+    let mut a = generate_array(size);
 
-    println!("Starting array: {:?}",b);
-    bubble_sort(&mut b);
-    println!("Final array {:?}", b);
-    //generate_random_array()
+    println!("Starting array: {:?}", a);
+    bubble_sort(&mut a);
+    println!("Final array {:?}", a);
 }
